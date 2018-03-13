16a01e6e4ed4b7100f5c0bf085062f2dffd3cdff
Aleksander Fret <alemik@op.pl>
2018-03-14 00:51:24 +0100

    Add functions to generate cards based on game settings

--- js/app.js
+++ js/app.js
@@ -74,4 +74,85 @@ const difficultyLevels = {
  */
 function gameInitHandler() {
   insertPageDate();
+  placeCards(reverse, obverse, level);
+}
+
+/**
+ * @description Insert year to the website footer
+ */
+function insertPageDate() {
+  const pageDate = document.querySelector('#years');
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const startYear = 2018;
+  if (year > startYear) {
+    pageDate.textContent = startYear + '–' + year;
+  } else {
+    pageDate.textContent = startYear;
+  }
+}
+
+/**
+ * @description place cards on the board
+ */
+function placeCards(reverseType, obverseType, Level) {
+  const reversTypeClass = reverseTypes[reverseType].class;
+  const cardsDirectory = obverseTypes[obverseType].directory;
+
+  const cards = shuffle(obverseTypes[obverseType].collection).splice(level-1);
+  const doubleShuffleCards = shuffle(cards.concat(cards));
+  const cardList = document.createDocumentFragment();
+
+  for (card of doubleShuffleCards) {
+    const newCard = createCard(card, reversTypeClass, cardsDirectory);
+    cardList.appendChild(newCard);
+  }
+
+  deck.appendChild(cardList);
+}
+
+/**
+ * @description Create card
+ * @param {string} card
+ * @param {string} reversTypeClass
+ * @param {string} directory
+ * @return {object}
+ */
+function createCard(card, reversTypeClass, directory) {
+  const newCardPlace = document.createElement('li');
+  newCardPlace.classList.add('card-place');
+
+  const newCard = document.createElement('div');
+  newCard.classList.add('card');
+
+  const newReverse = document.createElement('div');
+  newReverse.classList.add('reverse', reversTypeClass);
+  newReverse.setAttribute('data-card', card);
+
+  const newObverse = document.createElement('div');
+  newObverse.classList.add('reverse', 'obverse');
+  newObverse.setAttribute('data-card', card);
+
+  const cardImage = document.createElement('img');
+  cardImage.classList.add('card-image');
+  cardImage.setAttribute('src', ('img/' + directory + '/' + card + '.svg'));
+
+  newObverse.appendChild(cardImage);
+  newCard.appendChild(newReverse);
+  newCard.appendChild(newObverse);
+  newCardPlace.appendChild(newCard);
+
+  return newCardPlace;
+}
+
+/**
+ * @description Shuffle the list of cards
+ * @param {array} array
+ * @return {array}
+ */
+function shuffle(array) {
+  // https://css-tricks.com/snippets/javascript/shuffle-array/
+  return array.sort(function () {
+    return 0.5 - Math.random()
+  });
 }
