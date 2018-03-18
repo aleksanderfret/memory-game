8a6f6d1dd5e0182a4fc21ecd6b27c1ebebffb0ba
Aleksander Fret <alemik@op.pl>
2018-03-18 12:41:47 +0100

    docs: Add comments to the variables and functions

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const initialReverse = 'blue';
 const initialObverse = 'flags';
 const initialGameBackground = 'green';
 
-// game settings objects
+// game settings object
 const settings = {
   obverseTypes: {
     animals: {
@@ -91,17 +91,23 @@ const settings = {
   }
 }
 
-// set current variable based on initial variables
+// Sets current variable with initial values
 let level = initiaLevel;
 let reverse = initialReverse;
 let obverse = initialObverse;
 let gameBackground = initialGameBackground;
+// Stores the number of pairs remaining to be discovered
 let remainigPairs = settings.difficultyLevels[level];
+// Stores the first of the two fliped cards
 let currentFirstCard = null;
+// Lets block an event in some cases
 let locked = false;
+// Stores the number of clicks
 let clickCounter = 0;
+// Stores the number of moves
 let move = 0;
 
+// Variables needed to measure time
 let startTimestamp;
 let endTimeStamp;
 let interval;
@@ -110,6 +116,7 @@ let minutes = 0;
 let seconds = 0;
 let houndreth = 0;
 
+// Checks which property has to be used when animation ends
 const animationEnd = (function(element) {
   const animations = {
     animation: 'animationend',
@@ -126,7 +133,7 @@ const animationEnd = (function(element) {
 })(document.createElement('div'));
 
 /**
- * @description Initiate game
+ * @description Initiates the game
  */
 function gameInitHandler() {
   insertPageDate();
@@ -135,54 +142,64 @@ function gameInitHandler() {
 }
 
 /**
- * @description Insert year to the website footer
+ * @description Inserts current year to the website footer
  */
 function insertPageDate() {
   const pageDate = document.querySelector('#years');
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const startYear = 2018;
-  if (year > startYear) {
-    pageDate.textContent = startYear + '–' + year;
-  } else {
-    pageDate.textContent = startYear;
-  }
+  pageDate.textContent = (year > startYear) ? startYear + '–' + year : startYear;
 }
 
+/**
+ * @description Sets the game background
+ * @param {string} gameBackground
+ */
 function confiureGame(gameBackground) {
   const board = document.querySelector('body');
   const backgroundTypes = Object.keys(settings.gameBoard.background);
-
+  // Removes existing background classes
   for (backgroundType of backgroundTypes) {
     board.classList.remove(settings.gameBoard.background[backgroundType]);
   }
+  // Adds proper (current) background class
   board.classList.add(settings.gameBoard.background[gameBackground]);
 }
 
 /**
- * @description place cards on the board
+ * @description Puts the shuffled cards on the board
  * @param {string} reverseType
  * @param {string} obverseType
  * @param {string} level
  */
 function placeCards(reverseType, obverseType, Level) {
+  // Determines the number of pairs
   const pairsOfCards = settings.difficultyLevels[level];
+  // Determines proper reverses and directory
   const reversTypeClass = settings.reverseTypes[reverseType].class;
   const cardsDirectory = settings.obverseTypes[obverseType].directory;
+  // Randomly chooses cards and limits their number according to the game level
   const cards = shuffle(settings.obverseTypes[obverseType].collection).splice(0, pairsOfCards);
+  // Doubles the cards to creates pairs and shuffle them
   const doubleShuffleCards = shuffle(cards.concat(cards));
+  // Creates a new empty DocumentFragment
   const cardList = document.createDocumentFragment();
 
+  // For each card creates new DOM element and adds it to the DocumentFragment (cardList)
   for (card of doubleShuffleCards) {
     const newCard = createCard(card, reversTypeClass, cardsDirectory, level);
     cardList.appendChild(newCard);
   }
+  // Adds filled DocumentFragment (cardList) to the DOM
   deck.appendChild(cardList);
+
+  // Gives list the appropriate class based on the game level
   deck.classList.add(level+'-level-deck');
 }
 
 /**
- * @description Create card
+ * @description Creates card
  * @param {string} card
  * @param {string} reversTypeClass
  * @param {string} directory
@@ -190,97 +207,133 @@ function placeCards(reverseType, obverseType, Level) {
  * @return {object}
  */
 function createCard(card, reversTypeClass, directory, level) {
+  // Creates li - the card container
   const newCardPlace = document.createElement('li');
   newCardPlace.classList.add('card-place', level+'-level-place');
 
+  // Creates div - the card
   const newCard = document.createElement('div');
   newCard.classList.add('card');
 
+  // Creates div - the reverse of the card
   const newReverse = document.createElement('div');
   newReverse.classList.add('reverse', reversTypeClass);
   newReverse.setAttribute('data-card', card);
   newReverse.setAttribute('data-reverse', true);
 
+  // Creates div - the obverse of the card
   const newObverse = document.createElement('div');
   newObverse.classList.add('reverse', 'obverse');
   newObverse.setAttribute('data-card', card);
 
+  // Creates img - the image of the card
   const obverseImage = document.createElement('img');
   obverseImage.classList.add('card-image');
   obverseImage.setAttribute('src', ('img/' + directory + '/' + card + '.svg'));
 
-  //newReverse.appendChild(reversImage);
+  // Folds the card from individual elements
   newObverse.appendChild(obverseImage);
   newCard.appendChild(newReverse);
   newCard.appendChild(newObverse);
   newCardPlace.appendChild(newCard);
 
+  // returns card containeir
   return newCardPlace;
 }
 
 /**
- * @description Shuffle the list of cards
+ * @description Shuffles the list of cards
  * @param {array} array
  * @return {array}
+ * Inspired by/Taken from: https://css-tricks.com/snippets/javascript/shuffle-array/
  */
 function shuffle(array) {
-  // https://css-tricks.com/snippets/javascript/shuffle-array/
   return array.sort(function () {
     return 0.5 - Math.random()
   });
 }
 
-deck.addEventListener('click', function (event) {
+// Flips card on clic event
+deck.addEventListener('click', function flipCard(event) {
   if (!locked && event.target.dataset.reverse && event.target !== currentFirstCard){
+    // Checks whether to enable the countdown of time
     if (clickCounter === 0) {
       startTimer();
     }
+    // Counts clicks
     clickCounter++;
+    // Flips the card
     event.target.parentElement.classList.add('fliped-card');
+    // Checks which card was fliped
     if (!currentFirstCard) {
       currentFirstCard = event.target;
     } else {
+      // Counts moves and dispaly their number to the user
       move++;
       moveControl.textContent = move;
+      // Locks this eventlistener for checking time
       locked = true;
       setTimeout(function(){
         checkPair(currentFirstCard, event.target);
-      }, 1000);
+      }, 1200);
     }
   }
 });
 
+/**
+ * @description Checks whether the fliped cards are a pair
+ * @param {object} firstCard
+ * @param {object} secondCard
+ */
 function checkPair(firstCard, secondCard) {
+  // Gets data-card attribute to comparing cards
   const firstCardData = firstCard.getAttribute('data-card');
   const secondCardData = secondCard.getAttribute('data-card');
+  // Compares cards
   if (firstCardData !== secondCardData) {
+    // Covers fliped cards
     firstCard.parentElement.classList.remove('fliped-card');
     secondCard.parentElement.classList.remove('fliped-card');
   } else {
     firstCard.parentElement.classList.add('found-pair');
     secondCard.parentElement.classList.add('found-pair');
+    // Decreases the number of remaining pairs
     remainigPairs--;
     if(remainigPairs === 0) {
+      // Stops timer and end game
       stopTimer();
       finishGame();
     }
   }
+  // Clears first fliped card
   currentFirstCard = null;
+  // Unlocks eventListener
   locked = false;
 }
 
-
+/**
+ * @description Turns on the stopwatch and captures the initial timestamp
+ */
 function startTimer() {
   startTimestamp = Date.now();
   interval = setInterval(stopWatch, 10);
 }
 
+/**
+ * @description Turns off the stopwatch and captures the final timestamp
+ */
 function stopTimer() {
   endTimeStamp = Date.now();
   clearInterval(interval);
 }
 
+
+/**
+ * @description Measures the time of the game and displays it to the user
+ * Inspired by/Taken from: https://jsfiddle.net/Daniel_Hug/pvk6p/
+ */
 function stopWatch () {
+  // Calculates the time units
   houndreth++;
   if(houndreth >= 100) {
     houndreth = 0;
@@ -293,6 +346,7 @@ function stopWatch () {
       }
     }
   }
+  // Puts current time on the page
   const currentTime = ((hours < 10) ? "0" : "") + hours + ':'
                     + ((minutes < 10) ? "0" : "") + minutes + ':'
                     + ((seconds < 10) ? "0" : "") + seconds + ':'
@@ -300,6 +354,9 @@ function stopWatch () {
   timeControl.textContent = currentTime;
 }
 
+/**
+ * @description Ends game, resets timer and moves number and dispays modal
+ */
 function finishGame() {
   const gameTime = timeControl.textContent;
   const gameTimestampDiff = endTimeStamp - startTimestamp;
