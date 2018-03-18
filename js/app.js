b31f9f6a094aaaf66bf0cdb843847d490a23c2bc
Aleksander Fret <alemik@op.pl>
2018-03-18 03:00:54 +0100

    refactor: refactoring game settings

--- js/app.js
+++ js/app.js
@@ -11,83 +11,83 @@ const initialObverse = 'flags';
 const initialGameBackground = 'green';
 
 // game settings objects
-const obverseTypes = {
-  animals: {
-    id: 1,
-    label: 'animals',
-    directory: 'animals',
-    collection: ['ant', 'bat', 'bee', 'bird', 'buffalo', 'butterfly', 'camel', 'camel-1', 'cat', 'caterpillar',
-    'chipmunk', 'cow', 'crab', 'cricket', 'crocodile', 'dog', 'dolphin', 'dove', 'duck', 'eagle',
-    'elephant', 'ewe', 'fish', 'fish-1', 'goat', 'hedgehog', 'hen', 'horse', 'ladybug', 'lizard',
-    'monkey', 'mouse', 'octopus', 'ox', 'pig', 'rabbit', 'ram', 'rat', 'rhinoceros', 'scorpion',
-    'shark', 'shrimp', 'snail', 'snake', 'spider', 'tiger', 'turkey', 'turtle', 'whale', 'whale-1'],
-    license: 'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+const settings = {
+  obverseTypes: {
+    animals: {
+      id: 1,
+      label: 'animals',
+      directory: 'animals',
+      collection: ['ant', 'bat', 'bee', 'bird', 'buffalo', 'butterfly', 'camel', 'camel-1', 'cat', 'caterpillar',
+      'chipmunk', 'cow', 'crab', 'cricket', 'crocodile', 'dog', 'dolphin', 'dove', 'duck', 'eagle',
+      'elephant', 'ewe', 'fish', 'fish-1', 'goat', 'hedgehog', 'hen', 'horse', 'ladybug', 'lizard',
+      'monkey', 'mouse', 'octopus', 'ox', 'pig', 'rabbit', 'ram', 'rat', 'rhinoceros', 'scorpion',
+      'shark', 'shrimp', 'snail', 'snake', 'spider', 'tiger', 'turkey', 'turtle', 'whale', 'whale-1'],
+      license: 'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+    },
+    food: {
+      id: 2,
+      label: 'food',
+      directory: 'food',
+      collection: ['apple', 'baguette', 'banana', 'beans', 'biscuit', 'blueberries', 'boiled', 'bread', 'broccoli', 'cabbage',
+      'candy', 'carrot', 'cauliflower', 'cheese', 'cherries', 'chili', 'chocolate', 'cookies', 'corn', 'cucumber',
+      'doughnut', 'egg', 'garlic', 'grapes', 'hazelnut', 'honey', 'ice-cream', 'jam', 'lemon', 'milk',
+      'onion', 'orange', 'pancakes', 'peach', 'pear', 'peas', 'pineapple', 'pizza', 'potatoes', 'radish',
+      'raspberry', 'salad', 'salami', 'sausage', 'strawberry', 'toast', 'tomato', 'turkey', 'water', 'watermelon' ],
+      license: 'Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+    },
+    flags: {
+      id: 3,
+      label: 'flags',
+      directory: 'flags',
+      collection: ['algeria', 'argentina', 'australia', 'austria', 'belgium', 'brazil', 'cameroon', 'canada', 'chile', 'china',
+        'colombia', 'cuba', 'czechia', 'denmark', 'egypt', 'finland', 'france', 'germany', 'greece', 'india',
+        'ireland', 'israel', 'italy', 'jamaica', 'japan', 'mexico', 'morocco', 'netherlands', 'new-zealand', 'nigeria',
+        'norway', 'paraguay', 'peru', 'philippines', 'poland', 'portugal', 'russia', 'saudi-arabia', 'senegal', 'south-africa',
+        'spain', 'sweden', 'switzerland', 'tanzania', 'thailand', 'turkey', 'uganda', 'ukraine', 'united-kingdom', 'united-states'],
+      license: 'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+    },
+    christmas: {
+      id: 4,
+      label: 'christmas',
+      directory: 'christmas',
+      collection: ['bauble', 'bauble-1', 'bauble-2', 'bauble-3', 'bauble-4', 'bauble-5', 'bauble-6', 'bauble-7', 'bauble-8', 'bauble-9',
+      'bauble-10', 'bauble-11', 'bauble-12', 'bauble-13', 'baubles', 'bells', 'bells-1', 'bow', 'cabin-2', 'candle',
+      'candle-1', 'candle-2', 'candle-4', 'candy', 'candy-1', 'candy-2', 'candy-3', 'candy-4', 'candy-cane', 'candy-cane-1', 'candy-canes',
+      'christmas-card', 'christmas-card-1', 'christmas-sock', 'christmas-tree', 'christmas-wreath', 'christmas-wreath-1', 'garlands', 'gift', 'gift-1', 'gift-2',
+      'gift-3', 'gingerbread-man', 'lights', 'mistletoe', 'mistletoe-1', 'reindeer', 'santa-claus', 'snow-globe-1', 'snowman'],
+      license: 'Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+    }
   },
-  food: {
-    id: 2,
-    label: 'food',
-    directory: 'food',
-    collection: ['apple', 'baguette', 'banana', 'beans', 'biscuit', 'blueberries', 'boiled', 'bread', 'broccoli', 'cabbage',
-    'candy', 'carrot', 'cauliflower', 'cheese', 'cherries', 'chili', 'chocolate', 'cookies', 'corn', 'cucumber',
-    'doughnut', 'egg', 'garlic', 'grapes', 'hazelnut', 'honey', 'ice-cream', 'jam', 'lemon', 'milk',
-    'onion', 'orange', 'pancakes', 'peach', 'pear', 'peas', 'pineapple', 'pizza', 'potatoes', 'radish',
-    'raspberry', 'salad', 'salami', 'sausage', 'strawberry', 'toast', 'tomato', 'turkey', 'water', 'watermelon' ],
-    license: 'Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+  reverseTypes: {
+    blue: {
+      id: 1,
+      class: 'blue-reverse'
+    },
+    green: {
+      id: 1,
+      class: 'green-reverse'
+    },
+    red: {
+      id: 1,
+      class: 'red-reverse'
+    },
+    grey: {
+      id: 1,
+      class: 'grey-reverse'
+    }
   },
-  flags: {
-    id: 3,
-    label: 'flags',
-    directory: 'flags',
-    collection: ['algeria', 'argentina', 'australia', 'austria', 'belgium', 'brazil', 'cameroon', 'canada', 'chile', 'china',
-      'colombia', 'cuba', 'czechia', 'denmark', 'egypt', 'finland', 'france', 'germany', 'greece', 'india',
-      'ireland', 'israel', 'italy', 'jamaica', 'japan', 'mexico', 'morocco', 'netherlands', 'new-zealand', 'nigeria',
-      'norway', 'paraguay', 'peru', 'philippines', 'poland', 'portugal', 'russia', 'saudi-arabia', 'senegal', 'south-africa',
-      'spain', 'sweden', 'switzerland', 'tanzania', 'thailand', 'turkey', 'uganda', 'ukraine', 'united-kingdom', 'united-states'],
-    license: 'Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
+  difficultyLevels: {
+    easy: 8,
+    normal: 18,
+    hard: 32,
+    expert: 50
   },
-  christmas: {
-    id: 4,
-    label: 'christmas',
-    directory: 'christmas',
-    collection: ['bauble', 'bauble-1', 'bauble-2', 'bauble-3', 'bauble-4', 'bauble-5', 'bauble-6', 'bauble-7', 'bauble-8', 'bauble-9',
-    'bauble-10', 'bauble-11', 'bauble-12', 'bauble-13', 'baubles', 'bells', 'bells-1', 'bow', 'cabin-2', 'candle',
-    'candle-1', 'candle-2', 'candle-4', 'candy', 'candy-1', 'candy-2', 'candy-3', 'candy-4', 'candy-cane', 'candy-cane-1', 'candy-canes',
-    'christmas-card', 'christmas-card-1', 'christmas-sock', 'christmas-tree', 'christmas-wreath', 'christmas-wreath-1', 'garlands', 'gift', 'gift-1', 'gift-2',
-    'gift-3', 'gingerbread-man', 'lights', 'mistletoe', 'mistletoe-1', 'reindeer', 'santa-claus', 'snow-globe-1', 'snowman'],
-    license: 'Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
-  }
-};
-const reverseTypes = {
-  blue: {
-    id: 1,
-    class: 'blue-reverse'
-  },
-  green: {
-    id: 1,
-    class: 'green-reverse'
-  },
-  red: {
-    id: 1,
-    class: 'red-reverse'
-  },
-  grey: {
-    id: 1,
-    class: 'grey-reverse'
-  }
-};
-
-const difficultyLevels = {
-  easy: 8,
-  normal: 18,
-  hard: 32,
-  expert: 50
-};
-
-const gameBoard = {
-  background: {
-    blue: 'blue-board',
-    red: 'red-board',
-    green: 'green-board'
+  gameBoard: {
+    background: {
+      blue: 'blue-board',
+      red: 'red-board',
+      green: 'green-board'
+    }
   }
 }
 
@@ -96,7 +96,7 @@ let level = initiaLevel;
 let reverse = initialReverse;
 let obverse = initialObverse;
 let gameBackground = initialGameBackground;
-let remainigPairs = difficultyLevels[level];
+let remainigPairs = settings.difficultyLevels[level];
 let currentFirstCard = null;
 let locked = false;
 let clickCounter = 0;
@@ -151,12 +151,12 @@ function insertPageDate() {
 
 function confiureGame(gameBackground) {
   const board = document.querySelector('body');
-  const backgroundTypes = Object.keys(gameBoard.background);
+  const backgroundTypes = Object.keys(settings.gameBoard.background);
 
   for (backgroundType of backgroundTypes) {
-    board.classList.remove(gameBoard.background[backgroundType]);
+    board.classList.remove(settings.gameBoard.background[backgroundType]);
   }
-  board.classList.add(gameBoard.background[gameBackground]);
+  board.classList.add(settings.gameBoard.background[gameBackground]);
 }
 
 /**
@@ -166,10 +166,10 @@ function confiureGame(gameBackground) {
  * @param {string} level
  */
 function placeCards(reverseType, obverseType, Level) {
-  const pairsOfCards = difficultyLevels[level];
-  const reversTypeClass = reverseTypes[reverseType].class;
-  const cardsDirectory = obverseTypes[obverseType].directory;
-  const cards = shuffle(obverseTypes[obverseType].collection).splice(0, pairsOfCards);
+  const pairsOfCards = settings.difficultyLevels[level];
+  const reversTypeClass = settings.reverseTypes[reverseType].class;
+  const cardsDirectory = settings.obverseTypes[obverseType].directory;
+  const cards = shuffle(settings.obverseTypes[obverseType].collection).splice(0, pairsOfCards);
   const doubleShuffleCards = shuffle(cards.concat(cards));
   const cardList = document.createDocumentFragment();
 
